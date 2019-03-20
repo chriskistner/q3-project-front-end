36f2a798a6a2fe3ea3ebf5e7db6a78f89f559d78
Christopher Kistner <chris.kistner0@gmail.com>
2019-03-20 12:58:36 -0700

    Modified existing card search for to add type and color search parameters

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -10,9 +10,16 @@ class SearchForm extends Component {
 
         this.state = {
             name: '',
-            set: ''
+            set: '',
+            color: '',
         }
-    }
+    };
+
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        })
+    };
 
     handleChangeName = (event) => {
         this.setState({
@@ -24,17 +31,44 @@ class SearchForm extends Component {
         this.setState({
             set: event.target.value
         })
-    }
+    };
 
     render() {
         return (
         <div>
             <form className="form my-2 my-lg-0" onSubmit={(event) => { event.preventDefault()
             this.props.findCards(this.state.name, this.state.set)}}>
-                <div style={{marginBottom: 5}} className="form-group">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Enter Card's Name" name="bookID" value={this.state.name} onChange={this.handleChangeName} aria-label="Search"/>
+            <div className="row mb-2">
+                <div className="col">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search Cards by Name" name="name" value={this.state.name} onChange={this.handleChange} aria-label="Search"/>
                 </div>
-                <SetOptions name="set" set={this.state.set} changeSet={this.handleChangeSet} />
+            </div>
+            <div className="row mb-2">
+                <div className="col-6">
+                    <select className="form-control label-select" name="color" value={this.props.color} onChange={this.handleChange}>
+                        <option value="default" selected disabled>Select Card Color if Known</option>
+                        <option value="black">Black</option>
+                        <option value="blue">Blue</option>
+                        <option value="green">Green</option>
+                        <option value="red">Red</option>
+                        <option value="white">White</option>
+                    </select>
+                </div>
+                <div className="col-6">
+                    <select className="form-control label-select" name="type" value={this.props.color} onChange={this.handleChange}>
+                        <option value="default" selected disabled>Select Card Type if Known</option>
+                        <option value="artifact">Artifact</option>
+                        <option value="creature">Creature</option>
+                        <option value="enchantment">Enchantment</option>
+                        <option value="Instant">Instant</option>
+                        <option value="planeswalker">Planeswalker</option>
+                        <option value="spell">Spell</option>
+                        <option value="sorcery">Sorcery</option>
+                    </select>
+                </div>
+            </div>
+
+                <SetOptions name="set" set={this.state.set} changeSet={this.handleChange} />
                 <button className="btn btn-block btn-outline-success my-2 my-sm-0" type="submit">Find Card By Name</button>  
             </form>
         </div>
