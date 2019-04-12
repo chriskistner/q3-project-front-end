9459b5aad67e782cac354aa92b7ec27cf9f8e75e
Christopher Kistner <chris.kistner0@gmail.com>
2019-04-12 12:22:45 -0700

    Tweaked the Searched Card results form to add a search result count and usage message

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -6,10 +6,19 @@ function cardRow(props) {
     const cards = props.foundCards;
 
     return (
-        <div className="container" style={{display: "flex",flexDirection: "row", flexWrap: "wrap"}}>
-        {
+        <div className="container border">
+        {cards.length !== 0 ? <div className = "row align-items-center mt-1">
+            <div className = "col justify-content-center">
+                <p><strong>{cards.length} entries found:</strong> Click Card Image for More Stats</p>
+            </div>
+        </div> : null}
+        <div className="row align-items-center">
+            <div className="col" style={{display: "flex",flexDirection: "row", flexWrap: "wrap"}}>
+            {
             cards.slice(0,20).map(card => <CardData key={card.id} cardData={card} />)
             }
+            </div>
+        </div>
         </div>
     )
 
